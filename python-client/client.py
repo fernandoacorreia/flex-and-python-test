7e53152056839f5ff514ce4391b01e9519a14df0
Fernando Correia <fernandoacorreia@gmail.com>
2008-07-16 21:18:50 -0300

    Project objects insert and retrieval

--- python-client/client.py
+++ python-client/client.py
@@ -1,14 +1,71 @@
+"""Flex and Python Test client
+
+Usage:
+
+client.py test
+    Echo test.
+client.py insert 1 "Project name"
+    Inserts a new project with code = 1 and name = "Project name".
+client.py get 1
+    Gets a project with code = 1.
+client.py all
+    Lists all projects.
+"""
+
+import sys
 from pyamf.remoting.client import RemotingService
 
-gw = RemotingService('http://localhost:8080/')
+def usage():
+    print __doc__
 
-print "Testing EchoService:"
-service = gw.getService('EchoService')
-print service.echo('test')
-print service.echo_upper('test')
-print service.echo_upper(1)
+def test():
+    gw = RemotingService('http://localhost:8080/')
+    service = gw.getService('EchoService')
+    print service.echo('test')
+    print service.echo_upper('test')
+    print service.echo_upper(1)
 
-print "Testing ProjectService:"
-service = gw.getService('ProjectService')
-print service.get(1)
-print service.get_all()
+def insert(code, name):
+    gw = RemotingService('http://localhost:8080/')
+    service = gw.getService('ProjectService')
+    service.insert(int(code), name)
+
+def get(code):
+    gw = RemotingService('http://localhost:8080/')
+    service = gw.getService('ProjectService')
+    project = service.get(int(code))
+    if project == None:
+        print "Project %s not found." % (code)
+    else:
+        print_project(project)
+
+def all():
+    gw = RemotingService('http://localhost:8080/')
+    service = gw.getService('ProjectService')
+    projects = service.get_all()
+    for project in projects:
+        print_project(project)
+        
+def print_project(project):
+    print "Project code = %s, name = %s, created at %s" % (project.code, project.name, project.created_at)
+
+def main(args):
+    if len(args) == 0:
+        usage()
+        return 1
+    command = args[0]
+    if command == 'test' and len(args) == 1:
+        test()
+    elif command == 'insert' and len(args) == 3:
+        insert(args[1], args[2])
+    elif command == 'get' and len(args) == 2:
+        get(args[1])
+    elif command == 'all' and len(args) == 1:
+        all()
+    else:
+        usage()
+        return 1
+    return 0
+
+if __name__=='__main__':
+    sys.exit(main(sys.argv[1:]))
--- server/services/ProjectService.py
+++ server/services/ProjectService.py
@@ -1,10 +1,27 @@
+from google.appengine.ext import db
+
+class Project(db.Model):
+    code = db.IntegerProperty()
+    name = db.StringProperty()
+    created_at = db.DateTimeProperty(auto_now_add=True)
+
 class ProjectService:
-    def get(self, id):
+    def get(self, code):
+        project = Project.gql("WHERE code = :1", code).get()
+        if project is None:
+            return None
         project_vo = {
-            '_id': id,
-            'manager': 'john',
+            'code': project.code,
+            'name': project.name,
+            'created_at': project.created_at,
         }
         return project_vo
+        
+    def insert(self, code, name):
+        project = Project()
+        project.code = code
+        project.name = name
+        project.put()
 
     def get_all(self):
-        return [{'_id': 1, 'manager': 'jacob'}, {'_id': 2, 'manager': 'joseph'}]
+        return Project.all().fetch(1000)
